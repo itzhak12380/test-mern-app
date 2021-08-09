a888aef45609dbf5914cf3b8d8d13066407a7611
itzhak12380 <iascc12380@gmail.com>
2021-08-09 16:55:13 +0300

    heruko

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT=process.env.PORT||8080;
+const path  = require('path')
 const db = require('./DB/index')
 const exsprees=require('express');
 const cors = require('cors');
@@ -8,14 +9,15 @@ const mongodbClient = require('mongodb').MongoClient;
 const app = exsprees();
 app.use(exsprees.json());
 app.use(exsprees.urlencoded({extended:true}));
-const Blog = require('./Routes/Router')
+const Blog = require('./Routes/Router');
+const { patch } = require('./Routes/Router');
 app.use(cors());
 db.on('error',()=>{console.log('connection error')})
 app.listen(PORT,()=>{
     console.log(`server live on port: ${PORT}`);
 })
 app.use('/students',Blog)
-app.get('/',(req,res)=>{
-    res.send('succes');
-})
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(patch))
+}
 
