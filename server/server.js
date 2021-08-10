94b92c0982b2673f7cebc3042f58d72af3b3298b
itzhak12380 <iascc12380@gmail.com>
2021-08-10 09:41:49 +0300

    proxy changes

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ app.listen(PORT,()=>{
 })
 app.use('/students',Blog)
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(patch))
+    app.use(express.static(path.join(__dirname,'../client/build')))
+    
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../client/build','index.html'))
+    })
 }
 
