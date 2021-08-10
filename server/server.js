35b552af4192a6a204aadcc5237b7636b6d1012b
itzhak12380 <iascc12380@gmail.com>
2021-08-10 10:21:12 +0300

    heroku changes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT=process.env.PORT||8080;
 const path  = require('path')
 const db = require('./DB/index')
 const exsprees=require('express');
 const cors = require('cors');
 const mongodbClient = require('mongodb').MongoClient;
+const Blog = require('./Routes/Router');
+const { patch } = require('./Routes/Router');
+const PORT=process.env.PORT||8080;
 const app = exsprees();
 app.use(exsprees.json());
 app.use(exsprees.urlencoded({extended:true}));
-const Blog = require('./Routes/Router');
-const { patch } = require('./Routes/Router');
 app.use(cors());
 db.on('error',()=>{console.log('connection error')})
 app.listen(PORT,()=>{
