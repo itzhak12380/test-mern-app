4b7e624256ec31e1b5aa1e55d43a6244df5e610d
itzhak12380 <iascc12380@gmail.com>
2021-08-10 12:49:48 +0300

    sss

--- server/server.js
+++ server/server.js
@@ -7,15 +7,13 @@ const app = express();
 const db = require('./DB');
 const blogRouter = require('./Routes/Router')
 const path = require('path');
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 db.on('error', () => { console.log("connection error") })
-
 
 app.listen(PORT, () => {
     console.log(`mern server is live and up on port: ${PORT}`);
